31188f77416170a4a74d228e844f73c4deea5190
Ramdan Abdul Gani <121142467+RamdanAG@users.noreply.github.com>
2023-03-01 22:21:10 +0700

    Add files via upload

---
+++ Websiteform/RegisterLogin/Register.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOgin</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <h6>Selangkah Lagi</h6>
+        <h1>Masukan Kode OTP</h1>
+        <h4>Masukan Kode OTP yang di kirim ke alamat email Robynugraha@gmail.com</h4>
+    </div>
+    
+    <div>
+        <form action="post">
+            <tr>
+                <td><label for="password" ></label></td>
+                <td><input id="password" type="password" placeholder="password"></td>
+            </tr>
+            <input type="submit" value="Login" />
+        </form>
+    </div>
+    <img src="https://source.unsplash.com/random/635x805/?technology" alt="Gambar Random">
+</body>
+</html>
