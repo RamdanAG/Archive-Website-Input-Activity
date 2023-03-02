6d3d71b5a402d6b2a636dd2a4be0cc1e8d32f633
ObiKusakabe <103549681+ObiKusakabe@users.noreply.github.com>
2023-03-02 13:19:35 +0700

    Menambahkan Footer

---
+++ Footer/Footer.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>footer</title>
+    <link rel="stylesheet" type="text/css" href="../Style/FooterSky/style.css">
+</head>
+<body>
+    <footer>
+        <div class="container">
+            <div class="sec aboutus">
+                <h2>About Us</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quo hic libero placeat saepe architecto commodi, vero repellat totam eveniet veritatis corporis illo inventore exercitationem. Deserunt fugit iste mollitia doloribus.</p>
+                <ul class="sci">
+                    <li><a href=#"><i class="fa fa-instagram" aria-hidden="true"></i>
+                    </a></li>
+                    <li><a href="#"><i class="fa fa-facebook-official" aria-hidden="true"></i>
+                    </i>
+                    </a></li>
+                    <li><a href=#"><i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a></li>
+                    <li><a href=#"><i class="fa fa-youtube-play" aria-hidden="true"></i>
+                    </a></li>
+                </ul>
+            </div>
+            <div class="sec quicklinks">
+                <h2>quick links</h2>
+                <ul>
+                    <li><a href="">About</a></li>
+                    <li><a href="">FAQ</a></li>
+                    <li><a href="">Privacy Policy</a></li>
+                    <li><a href="">Help</a></li>
+                    <li><a href="">Terms & Consitions</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="sec contact">
+                <h2>Contact Info</h2>
+                <ul class="info">
+                    <li>
+                        <span><i class="fa fa-map-marker" aria-hidden="true"></i>
+                        </span>
+                        <span>batujajar <br> tengah<br> barat</span>
+                    </li>
+                    <li>
+                        <span><i class="fa fa-phone" aria-hidden="true"></i>
+                        </span>
+                       <P><a href="tel:11111111">0987654334</a><br><a href="tel:11111111">0987654334</a></P>
+                    </li>
+                    <li>
+                        <span><i class="fa fa-envelope" aria-hidden="true"></i>
+                        </span>>
+                        <p><a href="mailto">nagabonar@gmail.com</a></p>
+                    </li>
+                </ul>
+            </div>
+        </div> 
+    </footer>
+</body>
+</html>
