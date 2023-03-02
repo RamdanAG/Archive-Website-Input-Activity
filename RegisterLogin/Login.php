1f0e260178ad2bfd679b200079381ec5cac20444
ObiKusakabe <103549681+ObiKusakabe@users.noreply.github.com>
2023-03-02 11:32:55 +0700

    Membuat Style Landing Page Login

--- RegisterLogin/Login.php
+++ RegisterLogin/Login.php
@@ -5,36 +5,40 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOgin</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../styleLogin/Login.css">
+    <link rel="stylesheet" href="../resetCSS/reset.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;500;900&display=swap" rel="stylesheet">
 </head>
 <body>
-    <div>
+    <div class="Heading-Text">
         <h6>Di Mulai Dari Sini</h6>
         <h1>Selamat Pagi<span>.</span></h1>
-        <h4>Selesaikan login untuk mengakses <b>Dashbboard</b></h4>
+        <h4>Selesaikan login untuk mengakses <b class="boldtext">Dashboard</b></h4>
     </div>
     
-    <div>
+    <div class="forminput">
         <form action="post">
             <tr>
-                <td><label for="Username" ></label></td>
-                <td><input id="Username" type="text" placeholder="Username"></td>
+                <td><label for="Username" class="label-text"></label></td>
+                <td><input class="input1 bentuk hoverinput" id="Username" type="text" placeholder="Username"></td>
             </tr>
             <tr>
-                <td><label for="email" ></label></td>
-                <td><input id="email" type="email" placeholder="Email"></td>
+                <td><label for="email" class="label-text"></label></td>
+                <td><input class="input2 bentuk hoverinput" id="email" type="email" placeholder="Email"></td>
             </tr>
             <tr>
-                <td><label for="password" ></label></td>
-                <td><input id="password" type="password" placeholder="password"></td>
+                <td><label for="password" class="label-text"></label></td>
+                <td><input class="input3 bentuk hoverinput" id="password" type="password" placeholder="Password"></td>
             </tr>
             <tr>
-                <td><label for="password" ></label></td>
-                <td><input id="password" type="password" placeholder="password"></td>
+                <td><label for="kodePegawai" class="label-text"></label></td>
+                <td><input class="input4 bentuk hoverinput" id="KodePegawai" type="password" placeholder="Kode Pegawai"></td>
             </tr>
-            <input type="submit" value="Login" />
+            <input type="submit" value="Submit" class="input5 bentuk2"/>
         </form>
     </div>
-    <img src="https://source.unsplash.com/random/635x805/?technology" alt="Gambar Random">
+    <img src="https://source.unsplash.com/random/500x500/?technology" alt="Gambar Random">
 </body>
 </html>
--- RegisterLogin/Register.php
+++ RegisterLogin/Register.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LOgin</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../resetCSS/reset.css">
 </head>
 <body>
     <div>
