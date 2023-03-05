a3ddb99a108ab91140da8307cc275d689934bf3a
Ramdan Abdul Gani <121142467+RamdanAG@users.noreply.github.com>
2023-03-05 21:31:47 +0700

    Perubahan Code CSS

--- Footer/Footer.php
+++ Footer/Footer.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>footer</title>
-    <link rel="stylesheet" type="text/css" href="../Style/FooterSky/style.css">
+    <link rel="stylesheet" href="../Style/Style.css">
 </head>
 <body>
     <footer>
