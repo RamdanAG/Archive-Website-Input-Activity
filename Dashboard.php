9cd3d72b365a7173c14195c071c716b50ba1cb98
Ramdan Abdul Gani <121142467+RamdanAG@users.noreply.github.com>
2023-03-27 07:19:53 +0700

    mempebaiki kode php

--- Dashboard.php
+++ Dashboard.php
@@ -1,171 +1,81 @@
 <!DOCTYPE html>
-<!--=== Coding by CodingLab | www.codinglabweb.com === -->
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <!----======== CSS ======== -->
-    <link rel="stylesheet" href="Style/Dashboard.css">
-     
-    <!----===== Iconscout CSS ===== -->
-    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
-
-    <title>Admin Dashboard Panel</title> 
+    <title>Document</title>
+    <link rel="stylesheet" href="Style/Dashboard.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" />
 </head>
+
 <body>
-    <nav>
-        <div class="logo-name">
-            <div class="logo-image">
-                <img src="images/logo.png" alt="">
-            </div>
-
-            <span class="logo_name">CodingLab</span>
-        </div>
-
-        <div class="menu-items">
-            <ul class="nav-links">
-                <li><a href="#">
-                    <i class="uil uil-estate"></i>
-                    <span class="link-name">Dahsboard</span>
-                </a></li>
-                <li><a href="Input.php">
-                    <i class="uil uil-files-landscapes"></i>
-                    <span class="link-name">Input</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-chart"></i>
-                    <span class="link-name">Analytics</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-thumbs-up"></i>
-                    <span class="link-name">Like</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-comments"></i>
-                    <span class="link-name">Comment</span>
-                </a></li>
-                <li><a href="#">
-                    <i class="uil uil-share"></i>
-                    <span class="link-name">Share</span>
-                </a></li>
-            </ul>
-            
-            <ul class="logout-mode">
-                <li><a href="#">
-                    <i class="uil uil-signout"></i>
-                    <span class="link-name">Logout</span>
-                </a></li>
-
-                <li class="mode">
-                    <a href="#">
-                        <i class="uil uil-moon"></i>
-                    <span class="link-name">Dark Mode</span>
-                </a>
-
-                <div class="mode-toggle">
-                  <span class="switch"></span>
-                </div>
-            </li>
-            </ul>
-        </div>
-    </nav>
-
-    <section class="dashboard">
-        <div class="top">
-            <i class="uil uil-bars sidebar-toggle"></i>
-
-            <div class="search-box">
-                <i class="uil uil-search"></i>
-                <input type="text" placeholder="Cari Disini">
-            </div>
-            
-            <img src="images/profile.jpg" alt="">
-        </div>
-
-        <div class="dash-content">
-            <div class="activity">
-                <div class="title">
-                    <i class="uil uil-clock-three"></i>
-                    <span class="text">Aktivitas Terbaru</span>
-                </div>
-
-            <!-- batas 1 -->
-            <div class="activity-data">
-                <div class="data names">
-                    <?php
-                    $query = "SELECT name FROM nama_tabel";
-                    $result = mysqli_query($conn, $query);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<span class='data-list'>" . $row['name'] . "</span>";
-                    }
-                    mysqli_free_result($result);
-                    ?>
-                </div>
-            </div>
-
-            <!-- batas 2 -->
-            <div class="activity-data">
-                <div class="data email">
-                    <?php
-                    $query = "SELECT email FROM nama_tabel";
-                    $result = mysqli_query($conn, $query);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<span class='data-list'>" . $row['email'] . "</span>";
-                    }
-                    mysqli_free_result($result);
-                    ?>
-                </div>
-            </div>
-
-            <!-- batas 3 -->
-            <div class="activity-data">
-                <div class="data joined">
-                    <?php
-                    $query = "SELECT joined_date FROM nama_tabel";
-                    $result = mysqli_query($conn, $query);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<span class='data-list'>" . $row['joined_date'] . "</span>";
-                    }
-                    mysqli_free_result($result);
-                    ?>
-                </div>
-            </div>
-
-            <!-- batas 4 -->
-            <div class="activity-data">
-                <div class="data type">
-                    <?php
-                    $query = "SELECT user_type FROM nama_tabel";
-                    $result = mysqli_query($conn, $query);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<span class='data-list'>" . $row['user_type'] . "</span>";
-                    }
-                    mysqli_free_result($result);
-                    ?>
-                </div>
-            </div>
-
-            <!-- batas 5 -->
-            <div class="activity-data">
-                <div class="data status">
-                    <?php
-                    $query = "SELECT user_status FROM nama_tabel";
-                    $result = mysqli_query($conn, $query);
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo "<span class='data-list'>" . $row['user_status'] . "</span>";
-                    }
-                    mysqli_free_result($result);
-                    ?>
-                </div>
-            </div>
-
-                </div>
+    <div class="table">
+        <div class="table_header">
+            <p>product detail</p>
+            <div>
+                <input placeholder="product" />
+                <button class="add_new">+add New</button>
             </div>
         </div>
-    </section>
+        <?php
+        $sql = "SELECT * FROM kegiatan";
+        include 'function.php';
 
-    <script src="script.js"></script>
+        $result = mysqli_query($conn, $sql);
+
+        // Menampilkan data dalam tabel
+        echo '<div class="table_section">';
+            echo '<table>';
+                echo '<thead>';
+                    echo '<tr>';
+                        echo '<th>S. No</th>';
+                        echo '<th>Nama Kegiatan</th>';
+                        echo '<th>Tempat Kegiatan</th>';
+                        echo '<th>PDM</th>';
+                        echo '<th>Metude Kegiatan</th>';
+                        echo '<th>Tanggal Mulai</th>';
+                        echo '<th>Tanggal Akhir</th>';
+                        echo '<th>file1</th>';
+                        echo '<th>file2</th>';
+                        echo '<th>file3</th>';
+                        echo '<th>file4</th>';
+                        echo '<th>Hapus/Update</th>';
+                        echo '</tr>';
+                    echo '</thead>';
+                echo '<tbody>';
+
+                    if (mysqli_num_rows($result) > 0) {
+                    $no = 1;
+                    while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<tr>';
+                        echo '<td>' . $no++ . '</td>';
+                        echo '<td>' . $row['nama_kegiatan'] . '</td>'; // Ganti dengan kolom gambar Anda
+                        echo '<td>' . $row['tempat_kegiatan'] . '</td>'; // Ganti dengan kolom nama Anda
+                        echo '<td>' . $row['PDM'] . '</td>'; // Ganti dengan kolom email Anda
+                        echo '<td>' . $row['metode_kegiatan'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>' . $row['tanggal_mulai'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>' . $row['tanggal_akhir'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>' . $row['file1'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>' . $row['file2'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>' . $row['file3'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>' . $row['file4'] . '</td>'; // Ganti dengan kolom owner Anda
+                        echo '<td>';
+                            echo '<button><i class="fa-solid fa-pen-to-square"></i></button>';
+                            echo '<button><i class="fa-sharp fa-solid fa-trash"></i></button>';
+                            echo '</td>';
+                        echo '</tr>';
+                    }
+                    }
+
+                    echo '</tbody>';
+                echo '</table>';
+            echo '</div>';
+
+        mysqli_close($conn);
+        ?>
+    </div>
 </body>
+
 </html>
